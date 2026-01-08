04f038ff25d620f4d9df2e374ccfbbf596247647
Joseph <10326622@upsamail.edu.gh>
2026-01-08 19:23:30 +0100

    updated

--- alx-react-app/src/Footer.jsx
+++ alx-react-app/src/Footer.jsx
@@ -1,8 +1,9 @@
-function footer(){
-    return  (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
-}
-export default Footer
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
