3298c2aef19ab3564330253bf074454caa721ad7
Joseph <10326622@upsamail.edu.gh>
2026-01-09 18:44:50 +0100

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,21 +1,26 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import UserProfile from './components/UserProfile';
 
 function App() {
-  return(
+  return (
     <div>
-
+      <WelcomeMessage />
       <Header />
-       <MainContent />
-       <Footer />
+      
+      {/* Passing data via props to UserProfile */}
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
+      <MainContent />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
