de1e5344dba0a86524d05f7add69b6fd1467b376
Joseph <10326622@upsamail.edu.gh>
2026-01-18 08:10:46 +0100

    updated

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,9 +1,5 @@
-// src/UserContext.js
-import React from 'react';
+import { createContext } from 'react';
 
-// Step 1: Create a Context for user data
-// This will allow us to share user data across components without prop drilling
-const UserContext = React.createContext();
+const UserContext = createContext();
 
-// Export the context so other components can use it
 export default UserContext;
